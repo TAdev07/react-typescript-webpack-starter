7094a311357b3916793865dd86e1ccd01d2418e9
Tuấn Anh <anhnguyen@houze.group>
2022-02-24 17:15:17 +0700

    project init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+import IMAGE from "./react-typescript.png";
+
+export const App = () => {
+  return (
+    <div>
+      <h1>React TypeScript - developer {process.env.name}</h1>
+      <img src={IMAGE} alt="React TypeScript" width="400" height="200" />
+    </div>
+  );
+};
